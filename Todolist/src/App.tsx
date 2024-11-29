f9460bac7ad6803f70ec43b470b241c39d96b7f4
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2024-11-29 12:17:19 -0800

    gggg

--- Todolist/src/App.tsx
+++ Todolist/src/App.tsx
@@ -1,27 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Slider from './components/Slider'
-import InputBar from './components/InputBar'
-import Tasklist from './components/Tasklist'
-import Pined from './components/Pined'
+import "./App.css";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import InputBar from "./components/InputBar";
+import Tasklist from "./components/Tasklist";
+import EllipsisMenu from "./components/EllipsisMenu";
 
 function App() {
-   return (
+  return (
     <>
-    <div  className=' app'>
-    <Header />
-    <Slider/>
-    <InputBar/>
-    <Pined />
-   
-    <Tasklist />
-    <Tasklist /><Tasklist /><Tasklist /><Tasklist /><Tasklist /><Tasklist />
-    
-    </div>
-      
+      <div className=" app">
+        <Header />
+        <Slider />
+        <InputBar />
+        <Tasklist />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Todolist/src/components/Tasklist.tsx
+++ Todolist/src/components/Tasklist.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import "./tasklist.css"
+import "./tasklist.css";
+import pin from "../assets/pin.png";
+import React from "react";
+import EllipsisMenu from "./EllipsisMenu";
+
 export default function Tasklist() {
+  const [isPined, setPined] = React.useState(false);
   return (
-    <div className='tasklist'>
+    <div className="tasklist">
+      <div className="pinimage">
+        {isPined ? <img src={pin} alt="pin" className="pinimage" /> : " "}
+      </div>
 
-       <label className="custom-checkbox">
-  <input type="checkbox" />
-  <span className="checkmark"></span>
-  Stylish Checkbox
-</label>
+      <label className="custom-checkbox ">
+        <input type="checkbox" />
+        <span className="checkmark"></span>
+        Stylish Checkbox
+      </label>
 
-        </div>
-  )
+      <div className="ellipsis">
+        <i className="fas fa-ellipsis-h"></i>
+      </div>
+      <div className=" menuelipsi">
+        <EllipsisMenu />
+      </div>
+    </div>
+  );
 }
